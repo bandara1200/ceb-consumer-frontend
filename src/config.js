1e8ecdd2bd2741a3a3cd44386bfa1197d2f66560
bandara1200 <bandara@sergain.com>
2024-04-26 07:27:59 +0530

    inital commit

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+    "TOKEN_ENDPOINT": "https://35.187.254.163:9443/oauth2/token",
+    "AUTHORIZE_ENDPOINT": "https://35.187.254.163:9443/oauth2/authorize",
+    "RESPONSE_TYPE": "code",
+    "SCOPE": "openid profile roles email phone consumer_scope account_info_read user_mgt_read",
+    "REDIRECT_URI": "http://34.124.199.149:3000",
+    "CLIENT_ID": "F7nJfTUt6gdZZcnfqa7tFRj_4zYa",
+    "CLIENT_SECRET": "CdlPeGVUPjm9sTZsyx0LGcUq2twa",
+    "GRANT_TYPE": "authorization_code",
+    "CLIENT_URL": "http://34.124.199.149:3000",
+    "LOGOUT_URL": "https://35.187.254.163:9443/oidc/logout",
+    "REGISTER_ENDPOINT": "https://35.187.254.163:9443/accountrecoveryendpoint/register.do",
+    "TENET_DOMAIN": "carbon.super",
+    "COOKIE_PATH": "/"
+};
